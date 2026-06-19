8608dde3e0ab9a13b4ce778d4dbcf68e238eb6b3
penguin <paraslimbu7115@gmail.com>
2026-06-19 11:46:09 +0000

    feat: basic grep script

---
+++ grep.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/bash
+
+echo "Enter a file name to search for:"
+read file_name
+
+if [ -f "$file_name" ]; then
+    echo "File '$file_name' found."
+    echo "Enter a pattern to search for:"
+    read pattern
+    grep "$pattern" "$file_name"
+else
+    echo "File '$file_name' not found."
+fi
