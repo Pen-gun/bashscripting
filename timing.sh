4c873bc4f4ae7cc931ce0c55962412eeec2f21a1
penguin <paraslimbu7115@gmail.com>
2026-06-19 11:13:51 +0000

    using continious streaming every 3 sec

--- timing.sh
+++ timing.sh
@@ -1,2 +1,15 @@
 #! /usr/bin/bash
 
+echo "Enter any key to continue"
+
+while 'true'
+do
+    read -t 3 -n 1
+    if [ $? = 0 ]
+    then
+        echo "you have entered a key"
+        exit;
+    else
+        echo "waiting...."
+    fi
+done
