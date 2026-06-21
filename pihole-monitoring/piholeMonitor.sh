c04461b79813eee4971c8dcc5cb89ac82ea3175b
penguin <paraslimbu7115@gmail.com>
2026-06-21 10:55:23 +0000

    feat: upgrade, tolower case current time as it is my variable, 2. not assume docker start will start the container so if else check if the container really start 3. update path so it can run from anywhere and always gives output here

--- pihole-monitoring/piholeMonitor.sh
+++ pihole-monitoring/piholeMonitor.sh
@@ -6,14 +6,18 @@ CONTAINER_NAME="pihole"
 # Check if the container is running
 
 check=$(docker ps --filter "name=$CONTAINER_NAME" --filter "status=running" --format '{{.Names}}')
-Current_time=$(date)
+current_time=$(date)
 
 # Condition to check
 
 if [ -n "$check" ]; then
-	run="$Current_time: $check  is running"
+	run="$current_time: $check  is running"
 else
-	docker start $CONTAINER_NAME
-	run="$Current_time: $CONTAINER_NAME is starting"
+	if docker start "$CONTAINER_NAME" > /dev/null 2>&1; then
+        run="$current_time: $CONTAINER_NAME has been started successfully"
+    else
+        run="$current_time: Failed to start $CONTAINER_NAME"
+    fi
 fi
-echo "$run" >> output.txt 
+LOG_FILE="/home/penguin/desktop/bashScripting/pihole-monitoring/pihole.log"
+echo "$run" >> "$LOG_FILE"
